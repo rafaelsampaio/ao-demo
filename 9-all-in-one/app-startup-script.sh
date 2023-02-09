b1887c856f27f501541c98fb38e24900ee568308
Rafael Sampaio <rafael.sampaio@live.com>
2023-02-09 09:16:25 -0300

    update to latest bigip version

--- 9-all-in-one/app-startup-script.sh
+++ 9-all-in-one/app-startup-script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-docker run -dit -p 8000:3000 registry.hub.docker.com/bkimminich/juice-shop
+docker run -dit -p 8000:3000 registry.hub.docker.com/bkimminich/juice-shop:v14.4.0
 docker run -dit -p 8001:80 registry.hub.docker.com/vulnerables/web-dvwa
 docker run -dit -p 8002:80 registry.hub.docker.com/ianwijaya/hackazon
-docker run -dit -p 8003:8080 registry.hub.docker.com/webgoat/webgoat
+docker run -dit -p 8003:8080 registry.hub.docker.com/webgoat/webgoat:v8.1.0
+docker run -dit -p 8004:8080 registry.hub.docker.com/mendhak/http-https-echo:28
+
--- 9-all-in-one/outputs.tf
+++ 9-all-in-one/outputs.tf
@@ -4,3 +4,4 @@ output "bigip-mgmt-url" { value = "https://${google_compute_instance.f5_bigip.ne
 output "bigip-ext-ip" { value = google_compute_instance.f5_bigip.network_interface.1.access_config.0.nat_ip }
 output "juiceshop-ip" { value = google_compute_address.juiceshop.address }
 output "juiceshop-url" { value = "https://${google_compute_address.juiceshop.address}" }
+output "myip" { value = chomp(data.http.myip.response_body) }
